19d5f42c0d43ed33cadc1ee31473f40b7f679712
Daniel King <daniel.oliver.king@gmail.com>
2016-12-07 10:19:23 -0800

    Add initial migration to create tables for database

---
+++ db/migrations/20161207050718_articles.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('article', (table) => {
+      table.increments();
+      table.timestamp('published_at').defaultTo(knex.fn.now());
+
+      table.string('title').notNullable();
+      table.string('body').notNullable();
+      table.string('media_url');
+      table.integer('likes_count');
+      table.integer('author_id');
+    }),
+
+    knex.schema.createTable('author', (table) => {
+      table.increments();
+      table.string('name').notNullable();
+      table.string('icon_url');
+    }),
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('article'),
+    knex.schema.dropTable('author'),
+  ]);
+};
