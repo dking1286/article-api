10dfe1315d19d345b18cb22a4e9ba3569593c94a
Daniel King <daniel.oliver.king@gmail.com>
2016-12-07 03:48:05 -0800

    Add build tools, linting, and base dependencies

---
+++ Gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const watch = require('gulp-watch');
+const notify = require('gulp-notify');
+
+const file = require('file');
+
+const directories = {
+  SRC: './src',
+  DIST: './dist',
+};
+
+function handleErrors(...args) {
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>',
+  }).apply(this, args);
+  this.emit('end');
+}
+
+function isJavascript(filename) {
+  return /\.js$/.test(filename);
+}
+
+function transpileFiles(srcDirectory, distDirectory) {
+  file.walk(srcDirectory, (error, dirPath, dirs, files) => {
+    if (error) throw new Error(error);
+
+    files.forEach((filename) => {
+      if (!isJavascript(filename)) return;
+
+      gulp.src(filename)
+        .on('error', handleErrors)
+        .pipe(babel({
+          presets: ['es2015', 'airbnb', 'stage-0'],
+        }))
+        .pipe(gulp.dest(distDirectory));
+    });
+  });
+}
+
+
+gulp.task('transpile', () =>
+  transpileFiles(directories.SRC, directories.DIST));
+
+gulp.task('watch', () =>
+  watch('src', () => transpileFiles(directories.SRC, directories.DIST)));
+
+gulp.task('default', ['transpile', 'watch']);
