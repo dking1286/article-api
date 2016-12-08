85656c9c3ac4072ca78024f73608920968d70ce7
Daniel King <daniel.oliver.king@gmail.com>
2016-12-08 15:34:44 -0800

    Create seed data for database

---
+++ db/seeds/test/article_author_seed.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+
+exports.seed = function(knex, Promise) {
+  const exampleDataPath = path.join(
+    __dirname, '..', '..', 'sample_data', 'sample_data.json');
+
+  const exampleData = JSON.parse(fs.readFileSync(exampleDataPath));
+
+  return knex('author').del()
+    .then(() => knex('article').del())
+    .then(() => Promise.all(
+      exampleData.map(article => insertAuthorAndArticle(knex, article))
+    ));
+};
+
+function insertAuthorAndArticle(knex, article) {
+  const { title, body, media_url, likes_count, author } = article;
+  const { name, icon_url } = author;
+
+  return knex('author').insert({ name, icon_url })
+    .then(() => knex('author').select('id').where({ name }))
+    .then(([{ id }]) => {
+      return knex('article').insert({
+        title, body, media_url, likes_count, author_id: id,
+      });
+    });
+}
