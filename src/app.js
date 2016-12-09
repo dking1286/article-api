9fd297e4471d49efad1086b8fd9bb98d3c71feb2
Daniel King <daniel.oliver.king@gmail.com>
2016-12-08 19:36:34 -0800

    Add skeleton of routes, with middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,46 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+import { notFound, forbidden } from './middleware/default_responses';
+import { needOAuthToken } from './middleware/authentication';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+
+app.get('/articles', needOAuthToken, (req, res) => {
+  res.status(200).send();
 });
 
+app.post('/articles', needOAuthToken, (req, res) => {
+  res.status(200).send();
+});
+
+app.put('/articles', forbidden);
+
+app.delete('/articles', forbidden);
+
+
+
+app.get('/article', needOAuthToken, (req, res) => {
+  
+});
+
+app.post('/article', forbidden);
+
+app.put('/article', needOAuthToken, (req, res) => {
+
+});
+
+app.delete('/article', needOAuthToken, (req, res) => {
+
+});
+
+
+
+
+app.use((req, res) => {
+  res.status(404).send('The requested resource was not found');
+});
 
 export default app;
