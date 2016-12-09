ec6e48d8b896829468c84f13827eabd8b667134a
Daniel King <daniel.oliver.king@gmail.com>
2016-12-08 16:55:34 -0800

    Add tests for requiring authentication and on forbidden requests

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,15 +1,103 @@
 import test from 'tape';
+import request from 'supertest';
 
+import knex from '../db/knex';
 import app from '../src/index';
 
-test('this is a basic test', (assert) => {
-  console.log(process.env.NODE_ENV)
-  assert.equal(1, 2);
+function setup() {
+  return new Promise((resolve, reject) => {
+    knex.migrate.rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => knex.seed.run())
+    .then(() => resolve())
+    .catch(error => reject(error));
+  });
+}
 
+
+test('articles route protected on GET request', (assert) => {
+  setup().then(() => {
+    request(app)
+      .get('/articles')
+      .end((error, response) => {
+        const actual = response.status;
+        const expected = 401;
+
+        assert.equal(expected, actual,
+          'Response should have status 401 when oAuth token not provided');
+
+        assert.end();
+      });
+  })
+  .catch(error => assert.end(error));
+});
+
+test('articles route protected on POST request', (assert) => {
+  setup().then(() => {
+    request(app)
+      .post('/articles')
+      .end((error, response) => {
+        const actual = response.status;
+        const expected = 401;
+
+        assert.equal(expected, actual,
+          'Response should have status 401 when oAuth token not provided');
+
+        assert.end();
+      });
+  })
+  .catch(error => assert.end(error));
+});
+
+test('articles route responds 403 on PUT request', (assert) => {
+  setup().then(() => {
+    request(app)
+      .put('/articles')
+      .end((error, response) => {
+        const actual = response.status;
+        const expected = 403;
+
+        assert.equal(expected, actual,
+          'PUT request should be forbidden to /articles route');
+
+        assert.end();
+      });
+  })
+  .catch(error => assert.end(error));
+});
+
+test('responds 403 on DELETE request', (assert) => {
+  setup().then(() => {
+    request(app)
+      .delete('/articles')
+      .end((error, response) => {
+        const actual = response.status;
+        const expected = 403;
+
+        assert.equal(expected, actual,
+          'PUT request should be forbidden to /articles route');
+
+        assert.end();
+      });
+  })
+  .catch(error => assert.end(error));
+});
+
+
+test('article route protected on GET request', (assert) => {
   assert.end();
 });
 
-test('Closing server', (assert) => {
-  app.server.close();
+test('article route protected on PUT request', (assert) => {
   assert.end();
 });
+
+test('article route protected on DELETE request', (assert) => {
+  assert.end();
+});
+
+test('article route responds 403 on POST request', (assert) => {
+  assert.end();
+});
+
+
